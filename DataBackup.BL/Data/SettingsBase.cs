af68f496efc9274d8e8c7d3d60549497422d38c5
Kirill <kirill.vasil.oleg@gmail.com>
2022-08-25 15:56:24 +0300

    Add Base class

---
+++ DataBackup.BL/Data/SettingsBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup.Data
+{
+    public class SettingsBase
+    {
+        private const string filePath = @"backupTB.tb";
+        
+        private static SettingsBase _instance;
+
+        private SettingsBase()
+        {
+
+        }
+
+        private static SettingsBase GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SettingsBase();
+            }
+            return _instance;
+        }
+    }
+}
